3b2732d09e4c940a94d34a3b621f6ba2f5e3f113
Rakesh N Choudhry <rakeshnc@synopsys.com>
2021-07-06 18:32:36 +0530

    Initial version of webservice generated by generator-jhipster@7.1.0

---
+++ src/main/java/com/demo/webservice/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.demo.webservice.security;
+
+import com.demo.webservice.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
---
+++ src/test/java/com/demo/webservice/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.demo.webservice;
+
+import com.demo.webservice.WebserviceApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = WebserviceApp.class)
+public @interface IntegrationTest {
+}
